4021c66a961b4b85832915049eb09e499419f7e4
Alec Liu <sammiliu71@gmail.com>
2026-07-06 14:43:53 -0700

    Added chromatic abberation filter
    Implement function for chromatic abberation application. Clear issue with filter not rendering for full image for unknown reasons.

--- filterTest.js
+++ filterTest.js
@@ -11,7 +11,7 @@ const canvas = new fabric.Canvas("actual",
     });
 let fabricImg = "";
 let blurredImg = "";
-let clipPath, filterTarget;
+let clipPath, filterTarget, activeTarget;
     
 // const can = new fabric.Canvas("c", 
 //     {
@@ -24,31 +24,25 @@ let clipPath, filterTarget;
 
 fabric.enableGLFiltering = false;
 fabric.Canvas2dFilterBackend && (fabric.filterBackend = new fabric.Canvas2dFilterBackend());
+preload.style.display = "none";
 
 image.src = "test.jpg";
 image.onload = function() { // upon image loading, the canvas sets up dimensions to image dimesions
-    preload.width = image.naturalWidth; // then will draw the image via the context
-    preload.height = image.naturalHeight;
-    ctx.drawImage(image, 0, 0);
-    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-    preload.style.display = "none";
-
     fabricImg = new fabric.Image(image, {
-        left: 0,
-        top: 0,
+        left: window.innerWidth/2,
+        top: window.innerWidth/2,
         scaleX: 1,
         scaleY: 1
     });
     canvas.set("backgroundImage", fabricImg);
-    
+    // chromaAbberation(5, 1);
     blurredImg = new fabric.Image(image, {
-        left: 0,
-        top: 0,
+        left: window.innerWidth/2,
+        top: window.innerHeight/2,
         scaleX: 1,
         scaleY: 1,
         lockMovementX: true,
-        lockMovementY: true
+        lockMovementY: true,
     });
     blurredImg.hasControls = false;
     blurredImg.hasBorders = false;
@@ -56,6 +50,7 @@ image.onload = function() { // upon image loading, the canvas sets up dimensions
     blurring(blurredImg);
     blurredImg.clipPath = clipPath;
     canvas.sendObjectToBack(blurredImg);
+    activeTarget = blurredImg;
 
     canvas.requestRenderAll();
 }
@@ -76,23 +71,54 @@ clipPath = new fabric.Circle({
     top: filterTarget.top,
     left: filterTarget.left
 });
-// can.clipPath = clipPath;
-// can.requestRenderAll();
+
 filterTarget.on('moving', function() {
     console.log("filter moving");
     clipPath.top = filterTarget.top;
     clipPath.left = filterTarget.left;
     clipPath.setCoords();
-    blurredImg.dirty = true;
+    activeTarget.dirty = true; // somehow make it so that this can be changed at will, 
     canvas.requestRenderAll();
 });
 
-function chromaAbberation(imgData, intensity, phase) {
-    let data = imgData.data;
+function chromaAbberation(intensity, phase) {
+    /* steps for non vanilla canvas image modifications:
+        0. image data will have already been saved to imgData on image load
+        1. clear the hidden canvas
+        2. pull the data down and modify it
+            a. issue: how to keep an untouched variant of this?
+        3. put image data and then take the URL of it to use as a source to create a new fabricImage layer
+    */
+    preload.width = image.naturalWidth; // then will draw the image via the context
+    preload.height = image.naturalHeight;
+    ctx.drawImage(image, 0, 0);
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imageData.data;
+
     for(let i = phase % 4; i < data.length; i+=4) {
         data[i] = data[i + 4 * intensity]; // follows the matrix math-- aka, everything gets shifted over
     }
-    ctx.putImageData(imageData, 0, 0)
+
+    ctx.putImageData(imageData, 0, 0);
+    let chromaImg = new Image();
+    chromaImg.src = preload.toDataURL();
+    let chromaLayer = new fabric.Image(chromaImg, {
+        left: 0,
+        top: 0,
+        scaleX: 1,
+        scaleY: 1,
+        lockMovementX: true,
+        lockMovementY: true
+    });
+    chromaLayer.hasControls = false;
+    chromaLayer.hasBorders = false;
+    canvas.add(chromaLayer);
+    chromaLayer.clipPath = clipPath;
+    canvas.sendObjectToBack(chromaLayer);
+
+    canvas.requestRenderAll();
+    ctx.clearRect(0, 0, preload.width, preload.height);
+    activeTarget = chromaLayer;
 }
 
 function blurring(img) {
